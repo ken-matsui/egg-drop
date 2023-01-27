405f8f6c79cd0d8d8f75e999436e3611a4a4719d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-01-26 20:12:35 -0800

    Implement diagonal loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(int_roundings)]
 
-fn main() {
+#[allow(dead_code)]
+fn div_rounding() {
     #[allow(non_snake_case)]
     let N = 99_usize;
     let num_threads = 3_usize;
@@ -15,4 +16,44 @@ fn main() {
 
         println!("{from}..{to}");
     }
+}
+
+#[allow(dead_code)]
+fn diagonal_loop() {
+    #[allow(non_snake_case)]
+    let WIDTH: usize = 7;
+    #[allow(non_snake_case)]
+    let HEIGHT: usize = 3;
+    let mut ch = b'A';
+
+    let mut array: Vec<Vec<char>> = vec![vec![ch as char; WIDTH]; HEIGHT];
+    for i in 0..HEIGHT {
+        for j in 0..WIDTH {
+            array[i][j] = ch as char;
+            ch += 1;
+        }
+    }
+
+    for i in 0..HEIGHT {
+        for j in 0..WIDTH {
+            print!("{} ", array[i][j]);
+        }
+        println!();
+    }
+
+    println!();
+
+    for k in 0..=(WIDTH + HEIGHT - 2) {
+        for j in 0..=k {
+            let i = k - j;
+            if i < HEIGHT && j < WIDTH {
+                print!("{} ", array[i][j]);
+            }
+        }
+        println!();
+    }
+}
+
+fn main() {
+    diagonal_loop();
 }
