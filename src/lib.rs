25d204c730c694312bf15a50edb0a8e5aa029aff
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-01-27 01:34:43 -0800

    Fix variable names in simple_dp.rs

--- src/lib.rs
+++ src/lib.rs
@@ -12,17 +12,28 @@ pub use rayon_par_fast_dp::rayon_par_fast_dp;
 pub use rec::rec;
 pub use simple_dp::simple_dp;
 
+// LeetCode Constraints
+// assert!(1 <= n && n <= 100, "constraints for n: number of eggs");
+// assert!(1 <= h && h <= 10000, "constraints for h: number of floors");
+
 #[inline]
 fn precondition(n: i32, k: i32) {
     assert!(1 <= n, "constraints for n: number of eggs");
     assert!(1 <= k, "constraints for h: number of floors");
-    // LeetCode Constraints
-    // assert!(1 <= n && n <= 100, "constraints for n: number of eggs");
-    // assert!(1 <= h && h <= 10000, "constraints for h: number of floors");
+}
+#[inline]
+fn precondition2(n: usize, k: usize) {
+    assert!(1 <= n, "constraints for n: number of eggs");
+    assert!(1 <= k, "constraints for h: number of floors");
 }
 
 #[inline]
 pub fn egg_drop(f: fn(i32, i32) -> i32, n: i32, k: i32) -> i32 {
     precondition(n, k);
+    f(n, k)
+}
+#[inline]
+pub fn egg_drop2(f: fn(usize, usize) -> i32, n: usize, k: usize) -> i32 {
+    precondition2(n, k);
     f(n, k)
 }
--- src/simple_dp.rs
+++ src/simple_dp.rs
@@ -1,44 +1,41 @@
 use std::cmp::{max, min};
 
 // ref: https://en.wikipedia.org/wiki/Dynamic_programming#Egg_dropping_puzzle
-pub fn simple_dp(n: i32, k: i32) -> i32 {
-    #[allow(non_snake_case)]
-    let N = n as usize;
-    #[allow(non_snake_case)]
-    let K = k as usize;
-
-    let mut numdrops = vec![vec![0_i32; K + 1]; N + 1];
-
-    for i in 0..=k {
-        numdrops[1][i as usize] = i;
+#[allow(non_snake_case)]
+pub fn simple_dp(N: usize, K: usize) -> i32 {
+    let mut dp = vec![vec![0_i32; K + 1]; N + 1];
+    for i in 0..=K {
+        dp[1][i] = i as i32;
     }
 
-    for i in 2..=N {
-        for j in 1..=K {
+    for n in 2..=N {
+        for k in 1..=K {
             let mut minval = i32::MAX;
-            for x in 1..=j {
-                minval = min(minval, max(numdrops[i][j - x], numdrops[i - 1][x - 1]));
+            for x in 1..=k {
+                minval = min(minval, max(dp[n][k - x], dp[n - 1][x - 1]));
             }
-            numdrops[i][j] = 1 + minval;
+            dp[n][k] = 1 + minval;
         }
     }
-    numdrops[N][K]
+    dp[N][K]
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::egg_drop;
+    use crate::egg_drop2;
 
     #[test]
     fn test_dp() {
-        assert_eq!(egg_drop(simple_dp, 2, 1), 1);
-        assert_eq!(egg_drop(simple_dp, 1, 2), 2);
-        assert_eq!(egg_drop(simple_dp, 2, 6), 3);
-        assert_eq!(egg_drop(simple_dp, 3, 14), 4);
-        assert_eq!(egg_drop(simple_dp, 4, 30), 5);
-        assert_eq!(egg_drop(simple_dp, 5, 62), 6);
-        assert_eq!(egg_drop(simple_dp, 6, 126), 7);
-        assert_eq!(egg_drop(simple_dp, 50, 500), 9);
+        assert_eq!(egg_drop2(simple_dp, 2, 1), 1);
+        assert_eq!(egg_drop2(simple_dp, 1, 2), 2);
+        assert_eq!(egg_drop2(simple_dp, 2, 6), 3);
+        assert_eq!(egg_drop2(simple_dp, 3, 14), 4);
+        assert_eq!(egg_drop2(simple_dp, 4, 30), 5);
+        assert_eq!(egg_drop2(simple_dp, 5, 62), 6);
+        assert_eq!(egg_drop2(simple_dp, 6, 126), 7);
+        assert_eq!(egg_drop2(simple_dp, 4, 2), 2);
+        assert_eq!(egg_drop2(simple_dp, 8, 8), 4);
+        assert_eq!(egg_drop2(simple_dp, 50, 500), 9);
     }
 }
